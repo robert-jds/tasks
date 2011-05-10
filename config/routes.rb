ddb091c83eb116d5888ae7413146c643c0ba1193
Robert Sese <robert@jaguardesignstudio.com>
2011-05-10 12:45:38 -0700

    Moved to JQuery from Prototype, deleted default index page, and fleshed out layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Tasks::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  match '/about', :to => 'pages#/about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,6 +51,7 @@ Tasks::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
